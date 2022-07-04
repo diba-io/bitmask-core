89bc73bf3f43a325db94899a752fa006432998d3
Hunter Trujillo <cryptoquick@pm.me>
2022-07-04 15:43:40 -0600

    Update blind_utxo to use seal technique based on blind command implementation in rgb-std 0.8.0-rc.2

--- src/operations/rgb/receive_tokens.rs
+++ src/operations/rgb/receive_tokens.rs
@@ -1,16 +1,16 @@
 use anyhow::Result;
 use bitcoin::OutPoint;
-use commit_verify::commit_encode::CommitConceal;
+use bp::seals::txout::CloseMethod;
 use rgb::seal;
 
 use crate::{data::structs::BlindResponse, log};
 
 pub fn blind_utxo(utxo: OutPoint) -> Result<(BlindResponse, OutPoint)> {
-    let seal = seal::Revealed::from(utxo);
+    let seal = seal::Revealed::new(CloseMethod::TapretFirst, utxo);
 
     let result = BlindResponse {
         blinding: seal.blinding.to_string(),
-        conceal: seal.commit_conceal().to_string(),
+        conceal: seal.to_concealed_seal().to_string(),
     };
 
     log!(format!("blind result: {result:?}"));
