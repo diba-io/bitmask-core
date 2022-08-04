b5e2dc87c22daa8125dac0e5426b6a9c4106511c
Hunter Trujillo <cryptoquick@pm.me>
2022-08-04 14:29:42 -0600

    Add dotenv override. Update env vars to use the BitMask Esplora server.

--- src/data/constants.rs
+++ src/data/constants.rs
@@ -1,15 +1,20 @@
-use std::{str::FromStr, sync::RwLock};
+use std::{env, str::FromStr, sync::RwLock};
 
 use bitcoin::Network;
 use once_cell::sync::Lazy;
 
 fn dot_env(key: &str) -> String {
     let env_file = include_str!("../../.env");
-    if let Some(line) = env_file.split('\n').find(|e| e.starts_with(key)) {
-        let (_, val) = line.split_once('=').expect("value exists for key");
-        val.to_owned()
-    } else {
-        panic!("Couldn't access .env key: {}", key);
+    match env::var(key) {
+        Ok(val) => val,
+        Err(_) => {
+            if let Some(line) = env_file.split('\n').find(|e| e.starts_with(key)) {
+                let (_, val) = line.split_once('=').expect("value exists for key");
+                val.to_owned()
+            } else {
+                panic!("Couldn't access .env key: {}", key);
+            }
+        }
     }
 }
 
