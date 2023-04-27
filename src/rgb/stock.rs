22865ac5fa4e61a7b8fe5a38e081cf4adf7c0d7c
Hunter Trujillo <cryptoquick@pm.me>
2023-04-27 05:16:35 -0600

    Improvements to Stock serialization based upon Maxim's suggestions.

--- src/rgb/stock.rs
+++ src/rgb/stock.rs
@@ -1,4 +1,4 @@
-use amplify::confinement::Confined;
+use amplify::confinement::{Confined, U24};
 use anyhow::Result;
 use rgbstd::persistence::Stock;
 use strict_encoding::{StrictDeserialize, StrictSerialize};
@@ -6,10 +6,7 @@ use strict_encoding::{StrictDeserialize, StrictSerialize};
 use crate::carbonado::{retrieve, store};
 
 pub async fn store_stock(sk: &str, name: &str, stock: &Stock) -> Result<()> {
-    let data = stock
-        .to_strict_serialized::<{ usize::MAX }>()
-        .expect("")
-        .to_vec();
+    let data = stock.to_strict_serialized::<U24>()?;
 
     store(sk, name, &data).await
 }
@@ -19,8 +16,8 @@ pub async fn retrieve_stock(sk: &str, name: &str) -> Result<Stock> {
     if data.is_empty() {
         Ok(Stock::default())
     } else {
-        let confined = Confined::try_from_iter(data.iter().copied()).expect("");
-        let stock = Stock::from_strict_serialized::<{ usize::MAX }>(confined).expect("");
+        let confined = Confined::try_from_iter(data)?;
+        let stock = Stock::from_strict_serialized::<U24>(confined)?;
 
         Ok(stock)
     }
