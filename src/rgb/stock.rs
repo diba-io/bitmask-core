b99b6d46387d90953670bebb04cffacc5181a64e
Hunter Trujillo <cryptoquick@pm.me>
2023-04-28 19:03:23 -0600

    Use u32 to store Stock, so max Stock size is 4GB instead of just 16MB.

--- src/rgb/stock.rs
+++ src/rgb/stock.rs
@@ -1,4 +1,4 @@
-use amplify::confinement::{Confined, U24};
+use amplify::confinement::{Confined, U32};
 use anyhow::Result;
 use rgbstd::persistence::Stock;
 use strict_encoding::{StrictDeserialize, StrictSerialize};
@@ -6,7 +6,7 @@ use strict_encoding::{StrictDeserialize, StrictSerialize};
 use crate::carbonado::{retrieve, store};
 
 pub async fn store_stock(sk: &str, name: &str, stock: &Stock) -> Result<()> {
-    let data = stock.to_strict_serialized::<U24>()?;
+    let data = stock.to_strict_serialized::<U32>()?;
 
     store(sk, name, &data).await
 }
@@ -17,7 +17,7 @@ pub async fn retrieve_stock(sk: &str, name: &str) -> Result<Stock> {
         Ok(Stock::default())
     } else {
         let confined = Confined::try_from_iter(data)?;
-        let stock = Stock::from_strict_serialized::<U24>(confined)?;
+        let stock = Stock::from_strict_serialized::<U32>(confined)?;
 
         Ok(stock)
     }
