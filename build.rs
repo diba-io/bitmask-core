3f1fcc4fec9804fbcccd1b5eaa510810df18186a
Hunter Trujillo <cryptoquick@pm.me>
2023-07-13 12:01:19 -0600

    Add an RGB_LIB_IDs.toml file, and build script to auto-generate it.

---
+++ build.rs
@@ -0,0 +1,61 @@
+use std::{collections::BTreeMap, fs};
+
+use anyhow::Result;
+use rgbstd::{
+    interface::{LIB_ID_RGB20, LIB_ID_RGB21, LIB_ID_RGB25},
+    stl::{LIB_ID_RGB_CONTRACT, LIB_ID_RGB_STD},
+};
+use serde::{Deserialize, Serialize};
+
+type IdVersionMap = BTreeMap<String, String>;
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Serialize)]
+struct LibIds {
+    LIB_ID_RGB20: IdVersionMap,
+    LIB_ID_RGB21: IdVersionMap,
+    LIB_ID_RGB25: IdVersionMap,
+    LIB_ID_RGB_CONTRACT: IdVersionMap,
+    LIB_ID_RGB_STD: IdVersionMap,
+}
+
+const LIB_IDS_FILE: &str = "RGB_LIB_IDs.toml";
+const NOTE_COMMENT: &str =
+    "# Auto-generated semantic IDs for RGB consensus-critical libraries and their corresponding versions of bitmask-core.\n\n";
+
+fn main() -> Result<()> {
+    const BMC_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+    let lib_ids_file = fs::read_to_string(LIB_IDS_FILE)?;
+    let mut lib_ids: LibIds = toml::from_str(&lib_ids_file)?;
+
+    lib_ids
+        .LIB_ID_RGB20
+        .entry(LIB_ID_RGB20.to_owned())
+        .or_insert(BMC_VERSION.to_owned());
+
+    lib_ids
+        .LIB_ID_RGB21
+        .entry(LIB_ID_RGB21.to_owned())
+        .or_insert(BMC_VERSION.to_owned());
+
+    lib_ids
+        .LIB_ID_RGB25
+        .entry(LIB_ID_RGB25.to_owned())
+        .or_insert(BMC_VERSION.to_owned());
+
+    lib_ids
+        .LIB_ID_RGB_CONTRACT
+        .entry(LIB_ID_RGB_CONTRACT.to_owned())
+        .or_insert(BMC_VERSION.to_owned());
+
+    lib_ids
+        .LIB_ID_RGB_STD
+        .entry(LIB_ID_RGB_STD.to_owned())
+        .or_insert(BMC_VERSION.to_owned());
+
+    let toml = toml::to_string(&lib_ids)?;
+    fs::write(LIB_IDS_FILE, format!("{NOTE_COMMENT}{toml}"))?;
+
+    Ok(())
+}
--- tests/rgb/web/stl_ids.rs
+++ tests/rgb/web/stl_ids.rs
@@ -10,30 +10,30 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 async fn check_rgb20_stl() {
-    let shema = rgb20_stl();
-    assert_eq!(LIB_ID_RGB20, shema.id().to_string());
+    let schema = rgb20_stl();
+    assert_eq!(LIB_ID_RGB20, schema.id().to_string());
 }
 
 #[wasm_bindgen_test]
 async fn check_rgb21_stl() {
-    let shema = rgb21_stl();
-    assert_eq!(LIB_ID_RGB21, shema.id().to_string());
+    let schema = rgb21_stl();
+    assert_eq!(LIB_ID_RGB21, schema.id().to_string());
 }
 
 #[wasm_bindgen_test]
 async fn check_rgb25_stl() {
-    let shema = rgb25_stl();
-    assert_eq!(LIB_ID_RGB25, shema.id().to_string());
+    let schema = rgb25_stl();
+    assert_eq!(LIB_ID_RGB25, schema.id().to_string());
 }
 
 #[wasm_bindgen_test]
 async fn check_rgbstd_stl() {
-    let shema = rgb_std_stl();
-    assert_eq!(LIB_ID_RGB_STD, shema.id().to_string());
+    let schema = rgb_std_stl();
+    assert_eq!(LIB_ID_RGB_STD, schema.id().to_string());
 }
 
 #[wasm_bindgen_test]
 async fn check_rgbcontract_stl() {
-    let shema = rgb_contract_stl();
-    assert_eq!(LIB_ID_RGB_CONTRACT, shema.id().to_string());
+    let schema = rgb_contract_stl();
+    assert_eq!(LIB_ID_RGB_CONTRACT, schema.id().to_string());
 }
