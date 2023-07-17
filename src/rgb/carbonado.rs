401807c599c97239ced1beabe2415bf613f182cd
Armando Dutra <crisdut@outlook.com>
2023-07-17 16:12:24 -0300

    fix: carbonado error type check

--- src/rgb/carbonado.rs
+++ src/rgb/carbonado.rs
@@ -9,8 +9,24 @@ use crate::{
     rgb::{constants::RGB_STRICT_TYPE_VERSION, structs::RgbAccount},
 };
 
-pub async fn store_stock(sk: &str, name: &str, stock: &Stock) -> Result<()> {
-    let data = stock.to_strict_serialized::<U32>()?;
+#[derive(Debug, Clone, Eq, PartialEq, Display, From, Error)]
+#[display(doc_comments)]
+pub enum StorageError {
+    /// Retrieve '{0}' strict-encoding causes error. {1}
+    StrictRetrive(String, String),
+    /// Write '{0}' strict-encoding causes error. {1}
+    StrictWrite(String, String),
+    /// Retrieve '{0}' carbonado causes error. {1}
+    CarbonadoRetrive(String, String),
+    /// Write '{0}' carbonado causes error. {1}
+    CarbonadoWrite(String, String),
+}
+
+pub async fn store_stock(sk: &str, name: &str, stock: &Stock) -> Result<(), StorageError> {
+    let data = stock
+        .to_strict_serialized::<U32>()
+        .map_err(|op| StorageError::StrictWrite(name.to_string(), op.to_string()))?;
+
     store(
         sk,
         name,
@@ -19,10 +35,14 @@ pub async fn store_stock(sk: &str, name: &str, stock: &Stock) -> Result<()> {
         Some(RGB_STRICT_TYPE_VERSION.to_vec()),
     )
     .await
+    .map_err(|op| StorageError::CarbonadoWrite(name.to_string(), op.to_string()))
 }
 
-pub async fn force_store_stock(sk: &str, name: &str, stock: &Stock) -> Result<()> {
-    let data = stock.to_strict_serialized::<U32>()?;
+pub async fn force_store_stock(sk: &str, name: &str, stock: &Stock) -> Result<(), StorageError> {
+    let data = stock
+        .to_strict_serialized::<U32>()
+        .map_err(|op| StorageError::StrictWrite(name.to_string(), op.to_string()))?;
+
     store(
         sk,
         name,
@@ -31,22 +51,35 @@ pub async fn force_store_stock(sk: &str, name: &str, stock: &Stock) -> Result<()
         Some(RGB_STRICT_TYPE_VERSION.to_vec()),
     )
     .await
+    .map_err(|op| StorageError::CarbonadoWrite(name.to_string(), op.to_string()))
 }
 
-pub async fn retrieve_stock(sk: &str, name: &str) -> Result<Stock> {
-    let (data, _) = retrieve(sk, name).await.unwrap_or_default();
+pub async fn retrieve_stock(sk: &str, name: &str) -> Result<Stock, StorageError> {
+    let (data, _) = retrieve(sk, name)
+        .await
+        .map_err(|op| StorageError::CarbonadoRetrive(name.to_string(), op.to_string()))
+        .unwrap_or_default();
+
     if data.is_empty() {
         Ok(Stock::default())
     } else {
-        let confined = Confined::try_from_iter(data)?;
-        let stock = Stock::from_strict_serialized::<U32>(confined)?;
+        let confined = Confined::try_from_iter(data)
+            .map_err(|op| StorageError::StrictRetrive(name.to_string(), op.to_string()))?;
+        let stock = Stock::from_strict_serialized::<U32>(confined)
+            .map_err(|op| StorageError::StrictRetrive(name.to_string(), op.to_string()))?;
 
         Ok(stock)
     }
 }
 
-pub async fn store_wallets(sk: &str, name: &str, rgb_wallets: &RgbAccount) -> Result<()> {
-    let data = to_allocvec(rgb_wallets)?;
+pub async fn store_wallets(
+    sk: &str,
+    name: &str,
+    rgb_wallets: &RgbAccount,
+) -> Result<(), StorageError> {
+    let data = to_allocvec(rgb_wallets)
+        .map_err(|op| StorageError::StrictWrite(name.to_string(), op.to_string()))?;
+
     store(
         sk,
         name,
@@ -55,14 +88,20 @@ pub async fn store_wallets(sk: &str, name: &str, rgb_wallets: &RgbAccount) -> Re
         Some(RGB_STRICT_TYPE_VERSION.to_vec()),
     )
     .await
+    .map_err(|op| StorageError::CarbonadoWrite(name.to_string(), op.to_string()))
 }
 
-pub async fn retrieve_wallets(sk: &str, name: &str) -> Result<RgbAccount> {
-    let (data, _) = retrieve(sk, name).await.unwrap_or_default();
+pub async fn retrieve_wallets(sk: &str, name: &str) -> Result<RgbAccount, StorageError> {
+    let (data, _) = retrieve(sk, name)
+        .await
+        .map_err(|op| StorageError::CarbonadoRetrive(name.to_string(), op.to_string()))
+        .unwrap_or_default();
+
     if data.is_empty() {
         Ok(RgbAccount::default())
     } else {
-        let rgb_wallets = from_bytes(&data)?;
+        let rgb_wallets = from_bytes(&data)
+            .map_err(|op| StorageError::StrictRetrive(name.to_string(), op.to_string()))?;
         Ok(rgb_wallets)
     }
 }
