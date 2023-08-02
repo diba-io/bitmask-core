a16b40bf3c7ad5eb9488c6a3e099c4f70e1251d5
Hunter Trujillo <cryptoquick@pm.me>
2023-08-02 12:41:16 -0600

    Add debug statement. Bump version to 0.6.3-rc.1.

--- src/rgb/wallet.rs
+++ src/rgb/wallet.rs
@@ -21,6 +21,7 @@ use rgbstd::{
 use strict_encoding::tn;
 
 use crate::{
+    debug,
     rgb::{resolvers::ResolveSpent, structs::AddressTerminal},
     structs::{AllocationDetail, AllocationValue, UDAPosition, WatcherDetail},
 };
@@ -97,6 +98,8 @@ pub fn next_address(
         .max()
         .unwrap_or_default();
 
+    debug!(format!("Max RGB wallet derivations: {max}"));
+
     let next_index = max + 1;
     let scripts = wallet.descr.derive(iface_index, max..next_index);
     let addresses: Vec<AddressTerminal> = scripts
@@ -109,6 +112,8 @@ pub fn next_address(
             AddressTerminal { address, terminal }
         })
         .collect();
+
+    debug!(format!("RGB Addresses: {addresses:?}"));
 
     Ok(addresses[addresses.len() - 1].clone())
 }
