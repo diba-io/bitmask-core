b06e8d02907dc5f190e8d00bcfb30b5f1dd6e4f5
Hunter Trujillo <cryptoquick@pm.me>
2023-10-03 22:45:47 -0600

    Change lib to use web instead of just ts in case we want to support node.

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 wasm-pack build --release --target web
-mv pkg/*.ts lib/ts
-mv pkg/*.js lib/ts
-mv pkg/*.wasm lib/ts
-mv pkg/*.wasm.d.ts lib/ts
+mv pkg/*.ts lib/web
+mv pkg/*.js lib/web
+mv pkg/*.wasm lib/web
+mv pkg/*.wasm.d.ts lib/web
+mv pkg/README.md lib/web
